76c59ef53b29e3b94f39b1195ea582cee68611b5
Lea Salikov <144378042+leasalikov@users.noreply.github.com>
2025-01-26 14:42:32 +0200

    Add files via upload

---
+++ pracTek/fronted/vite-project/src/components/CeateCompany.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const SignUp = () => {
+  return (
+<p>Sign Up</p>
+  );
+};
+
+export default SignUp;
